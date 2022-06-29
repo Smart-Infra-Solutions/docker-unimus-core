6c056d1f641aefc69f1c818736d525b2d0ed0ea9
Frederic Senchet <fs@si.solutions>
2022-06-29 18:50:12 +0200

    provide the good agent

--- files/start.sh
+++ files/start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-java -javaagent:apm-agent-java.jar \
+java -javaagent:elastic-apm-agent.jar \
 -Delastic.apm.service_name=Unimus \
--Delastic.apm.application_packages=unimus.si.solutions \
+-Delastic.apm.environment=production \
+-Delastic.apm.application_packages=org.unimus.si.solutions \
 -Delastic.apm.server_url=http://10.114.16.8:8200 \
 -jar Unimus-Core.jar
