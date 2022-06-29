a727c6d4f04e681de0866d06fe3c2000cca81f7e
Frederic Senchet <fs@si.solutions>
2022-06-29 19:10:42 +0200

    Update start.sh

--- files/start.sh
+++ files/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 java -javaagent:elastic-apm-agent.jar \
--Delastic.apm.service_name=Unimus \
+-Delastic.apm.service_name=Unimus-core \
 -Delastic.apm.environment=production \
 -Delastic.apm.application_packages=org.unimus.si.solutions \
 -Delastic.apm.server_url=http://10.114.16.8:8200 \
