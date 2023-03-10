7f59baa661634f5bd396091eb434896ea1351536
Frederic Senchet <fred.chinchin@gmail.com>
2023-03-10 15:02:12 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Docker-unimus-core
+
+[![Build Status](https://drone.si.solutions/api/badges/SmartInfraSolutions/docker-unimus-core/status.svg)](https://drone.si.solutions/SmartInfraSolutions/docker-unimus-core)
+
+**Unimus core** container, should be run with [Docker-Unimus](https://hub.docker.com/r/smartinfrasolutions/unimus)
+
+## Configuration
+
+Check the [docker-compose](https://github.com/Smart-Infra-Solutions/docker-unimus-core/blob/main/docker-compose.yml) file for available options.
+
+- `UNIMUS_SERVER_ADDRESS=xx.xx.xx.xx` - the IP address or DNS name of the unimus server
+- `UNIMUS_SERVER_PORT=5509` - the core connection port of the unimus server
+- `UNIMUS_SERVER_ACCESS_KEY=......` - a very-very-very long string, this is the access token that you can copy from the Zone menu of Unimus Server Web under the "Remote core access key" option
+
+## Versions
+
+Alpine 3.17  
+Debian 11.6 slim  
+
+Unimus 2.3.2
+
+## Supported architectures
+
+arm64, amd64  
+
+## Links
+
+<https://hub.docker.com/r/smartinfrasolutions>  
+<https://github.com/Smart-Infra-Solutions/docker-unimus>  
---
+++ files/start.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+JAVA_EXTRA_PARAMS=""
+
+CONFIG_DIR="/etc/unimus-core"
+CONFIG_FILE="${CONFIG_DIR}/unimus-core.properties"
+
+# create conf dir
+[ ! -d "$CONFIG_DIR" ] && { mkdir -p $CONFIG_DIR; }
+
+# default value of unimus server port
+[ -z "$UNIMUS_SERVER_ADDRESS" ] && { UNIMUS_SERVER_ADDRESS=172.17.0.1; }
+[ -z "$UNIMUS_SERVER_PORT" ] && { UNIMUS_SERVER_PORT=8085; }
+[ -z "$UNIMUS_SERVER_ACCESS_KEY" ] && { UNIMUS_SERVER_ACCESS_KEY="NOT_A_VALID_ACCESS_KEY"; }
+
+[ ! -z "$XMS" ] && { JAVA_EXTRA_PARAMS="$JAVA_EXTRA_PARAMS -Xms$XMS"; }
+[ ! -z "$XMX" ] && { JAVA_EXTRA_PARAMS="$JAVA_EXTRA_PARAMS -Xmx$XMX"; }
+[ ! -z "$JAVA_OPTS" ] && { JAVA_EXTRA_PARAMS="$JAVA_OPTS"; }
+
+[ -z "$TZ" ] && { TZ="UTC"; }
+ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
+
+
+[ ! -f $CONFIG_FILE ] && { touch $CONFIG_FILE; }
+
+[ $( grep -i "unimus.address=" $CONFIG_FILE | wc -l ) -eq 0 ] && { echo "unimus.address=" >> $CONFIG_FILE; }
+[ ! -z $UNIMUS_SERVER_ADDRESS ] && { echo "Updating unimus.address in config ..."; sed -i s@unimus.address=.*@unimus.address=$UNIMUS_SERVER_ADDRESS@g $CONFIG_FILE; }
+
+[ $( grep -i "unimus.port=" $CONFIG_FILE | wc -l ) -eq 0 ] && { echo "unimus.port=" >> $CONFIG_FILE; }
+[ ! -z $UNIMUS_SERVER_PORT ] && { echo "Updating unimus port in config..."; sed -i s@unimus.port=.*@unimus.port=$UNIMUS_SERVER_PORT@g $CONFIG_FILE; }
+
+# remove multiple config lines
+sed '/unimus.access.key/d' $CONFIG_FILE
+#[ ! -z $UNIMUS_SERVER_ACCESS_KEY ] && { echo "Updating access key in config..."; sed -i s@unimus.access.key=.*@unimus.access.key=$UNIMUS_SERVER_ACCESS_KEY@g $CONFIG_FILE; }
+echo "unimus.access.key=$UNIMUS_SERVER_ACCESS_KEY" >> $CONFIG_FILE
+
+# verify jar
+if [ $( which jarsigner | wc -l ) -gt 0 ]
+then
+  jarsigner -verify /opt/unimus-core.jar | grep -i "jar verified" || { echo "Unimus Core binary is not verified"; exit 1; }
+fi
+
+# run
+java $JAVA_EXTRA_PARAMS -jar /opt/unimus-core.jar
